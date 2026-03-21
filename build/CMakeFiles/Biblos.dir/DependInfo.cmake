2475a5460b0c09c3ad4f001fe3a1bd460e8e9648
Zuzanna <zbieszczad@interia.pl>
2026-03-21 17:39:02 +0100

    minimal working version

---
+++ build/CMakeFiles/Biblos.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/PC/Desktop/ceplusplus_2/project_event_bus/source/Enemy.cpp" "CMakeFiles/Biblos.dir/source/Enemy.cpp.obj" "gcc" "CMakeFiles/Biblos.dir/source/Enemy.cpp.obj.d"
+  "C:/Users/PC/Desktop/ceplusplus_2/project_event_bus/source/EventBus.cpp" "CMakeFiles/Biblos.dir/source/EventBus.cpp.obj" "gcc" "CMakeFiles/Biblos.dir/source/EventBus.cpp.obj.d"
+  "C:/Users/PC/Desktop/ceplusplus_2/project_event_bus/source/LootSystem.cpp" "CMakeFiles/Biblos.dir/source/LootSystem.cpp.obj" "gcc" "CMakeFiles/Biblos.dir/source/LootSystem.cpp.obj.d"
+  "C:/Users/PC/Desktop/ceplusplus_2/project_event_bus/source/Player.cpp" "CMakeFiles/Biblos.dir/source/Player.cpp.obj" "gcc" "CMakeFiles/Biblos.dir/source/Player.cpp.obj.d"
+  "C:/Users/PC/Desktop/ceplusplus_2/project_event_bus/source/ScoreSystem.cpp" "CMakeFiles/Biblos.dir/source/ScoreSystem.cpp.obj" "gcc" "CMakeFiles/Biblos.dir/source/ScoreSystem.cpp.obj.d"
+  "C:/Users/PC/Desktop/ceplusplus_2/project_event_bus/source/SoundSystem.cpp" "CMakeFiles/Biblos.dir/source/SoundSystem.cpp.obj" "gcc" "CMakeFiles/Biblos.dir/source/SoundSystem.cpp.obj.d"
+  "C:/Users/PC/Desktop/ceplusplus_2/project_event_bus/source/main.cpp" "CMakeFiles/Biblos.dir/source/main.cpp.obj" "gcc" "CMakeFiles/Biblos.dir/source/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
